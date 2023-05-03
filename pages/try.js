23c3bfc6caef2ccc4c30681ec9366a81bc3fecc0
hasanelahi <sheikh.hasan7@gmail.com>
2023-05-03 21:48:15 +0200

    upload image functionality

--- pages/try.js
+++ pages/try.js
@@ -1,70 +1,117 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function Try() {
-  const [imageUrl, setImageUrl] = useState('');
-  const [result, setResult] = useState('');
+  const [image, setImage] = useState(null);
+  const [result, setResult] = useState(null);
+  const [isImageUploaded, setIsImageUploaded] = useState(false);
 
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
-
-    reader.onload = (event) => {
-      setImageUrl(event.target.result);
-    };
-
+  
     reader.readAsDataURL(file);
+    reader.onload = () => {
+      setImage(reader.result);
+      setIsImageUploaded(true); // Set isImageUploaded state to true
+      // Make API call to analyze image and set result state
+      // For this example, we'll just set a dummy result
+      setResult({
+        tumorDetected: true,
+        confidence: 0.9,
+        message: "A tumor has been detected in the uploaded image",
+      });
+    };
   };
-
-  const handleImageAnalysis = () => {
-    // TODO: Use ML model to analyze image and set result
-    setResult('Tumor detected');
+  const reset = () => {
+    setImage(null);
+    setResult(null);
+    setIsImageUploaded(false);
   };
-
-  return (
-    <div className="h-screen flex flex-col lg:flex-row">
-      <div className="w-full lg:w-1/2 bg-gray-200 flex items-center justify-center">
-        {imageUrl ? (
-          <img src={imageUrl} alt="Uploaded Image" className="w-full" />
-        ) : (
-          <label className="flex flex-col items-center justify-center w-full h-full bg-gray-300 text-gray-500 cursor-pointer hover:text-gray-600">
-            <svg
-              className="w-8 h-8 mb-2"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+return (
+  <div className="flex flex-col items-center justify-center mt-3">
+    <div className="w-full max-w-3xl">
+      <h1 className="text-3xl text-center font-bold mb-2">
+        Brain Tumor Detection System
+      </h1>
+      <p className="text-gray-600 text-center mb-12">
+        Upload an image to analyze for brain tumor detection
+      </p>
+      <div className="flex flex-col md:flex-row justify-center items-center">
+        <div className={`w-full md:w-1/2 flex items-center max-w-md ml-2  justify-center ${result ? 'md:mr-4' : ''}`}>
+          {image ? (
+            <img src={image} alt="Uploaded" className="max-w-md rounded-lg mr-10" />
+          ) : (
+            <div
+              className="w-full h-96 border-4 border-dashed border-gray-400 flex text-center justify-center cursor-pointer rounded-lg"
+              onClick={() => {
+                document.getElementById("image-input").click();
+              }}
+              style={{ display: "flex", alignItems: "center" }}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-              />
-            </svg>
-            <span className="text-sm">Upload an image</span>
-            <input
-              type="file"
-              className="hidden"
-              accept="image/*"
-              onChange={handleImageUpload}
-            />
-          </label>
-        )}
-      </div>
-      <div className="w-full lg:w-1/2 bg-gray-100 flex items-center justify-center">
+              <div className="text-gray-600 flex flex-col items-center justify-center">
+                <div className=" text-sm mb-3">
+                  <p className="font-semibold text-center">
+                    Drag and drop an image here or click to select an image
+                  </p>
+                  <input
+                    type="file"
+                    id="image-input"
+                    className="hidden"
+                    onChange={handleImageUpload}
+                  />
+                </div>
+                <svg
+                  className="w-12 h-12"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                  />
+                </svg>
+              </div>
+            </div>
+          )}
+        </div>
         {result ? (
-          <div>{result}</div>
+          <div className={`w-full md:w-1/2 md:mx-4 mt-4 md:mt-0`}>
+          <div className="bg-white ml-10 rounded-lg shadow p-4">
+            <p className="text-lg font-semibold mb-2">
+              Result:{" "}
+              {result.tumorDetected
+                ? "Tumor Detected"
+                : "No Tumor Detected"}
+            </p>
+            <p className="mb-2">
+              Confidence: {result.confidence.toFixed(2) * 100}%{" "}
+            </p>
+            <p className="text-gray-600">{result.message}</p>
+          </div>
+          <div className="text-center mt-4">
+            <button
+              className="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              onClick={reset}
+            >
+              Reset
+            </button>
+          </div>
+        </div>
+        
+          
         ) : (
-          <button
-            className="py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white rounded shadow"
-            onClick={handleImageAnalysis}
-          >
-            Analyze
-          </button>
+          false
         )}
+  
       </div>
     </div>
-  );
-}
+  </div>
+);
 
+
+}
 export default Try;
